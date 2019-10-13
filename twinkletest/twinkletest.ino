c240c8b2e01dcb795f18588715607ae361de4f0f
d13g4 <diemo@luisental.org>
2019-10-13 18:56:16 +0200

    added twinkletest

---
+++ twinkletest/twinkletest.ino
@@ -0,0 +1,114 @@
+#include <FastLED.h>
+
+FASTLED_USING_NAMESPACE
+
+// FastLED "100-lines-of-code" demo reel, showing just a few 
+// of the kinds of animation patterns you can quickly and easily 
+// compose using FastLED.  
+//
+// This example also shows one easy way to define multiple 
+// animations patterns and have them automatically rotate.
+//
+// -Mark Kriegsman, December 2014
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    3
+//#define CLK_PIN   4
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define NUM_LEDS    300
+CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          255
+#define FRAMES_PER_SECOND  120
+
+void setup() {
+  delay(3000); // 3 second delay for recovery
+  
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { fillall };
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+int delaycounter = 0; //counter for doing stuff frame-exact (coupled with FRAMES_PER_SECOND)
+void loop()
+{
+  // Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  EVERY_N_MILLISECONDS( 100 ) { (leds[NUM_LEDS] /=4);}
+  counter();
+}
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void fillall()
+{
+  fill_solid( leds, NUM_LEDS, CRGB::Red);
+}
+
+void twinkle()
+{
+ if (delaycounter ==60)
+  {
+   leds[NUM_LEDS]*=1; 
+  }
+ if (delaycounter == 0)
+ {
+   leds[NUM_LEDS]/=4;
+ }
+}
+void counter()
+{
+  if (delaycounter<FRAMES_PER_SECOND)   //start a counter resetting every second
+  {
+    delaycounter++;
+  }
+  else
+  {
+    delaycounter=0;
+  }
+}
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  if ((delaycounter % 2)==0)    //using delaycounter modulo 4, so that every 1/4th second the function will be computed
+  {
+    int pos = random16(NUM_LEDS);
+    leds[pos] += CHSV( gHue + random8(64), 200, 255);
+    leds[pos].subtractFromRGB(127);
+    leds[pos].subtractFromRGB((random16(32)+32));
+  }
+  if ((delaycounter % 8)==0)
+  {
+    fadeToBlackBy( leds, NUM_LEDS, 1);
+  }
+  
+}
+
